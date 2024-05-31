9e2f26ed0f8ded75e2216e9c877741b02b1b05b7
ManishSolanki.Khimjibhai <ManishSolanki.Khimjibhai@ust.com>
2024-05-31 09:20:04 +0530

    Project SetUp

---
+++ src/main/java/org/Manish/Main.java
@@ -0,0 +1,11 @@
+package org.Manish;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Map<String,String> m1 = new HashMap<>();
+    }
+}
